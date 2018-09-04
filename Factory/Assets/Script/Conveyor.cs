ad899dc3ae511b2dad9882f5a0709468bf040a61
Tamiwo <taimwo.otutama@gmail.com>
2018-09-05 00:42:44 +0900

    fix:回転がうまくいかない

--- Factory/Assets/Script/Conveyor.cs
+++ Factory/Assets/Script/Conveyor.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class Conveyor : MonoBehaviour {
 
     public float speed = 0.01f;
+    Vector3 dir;
 
 	// Use this for initialization
 	void Start () {
-		
+        float roll = transform.localRotation.y;
+        Vector3 vec = new Vector3( speed, 0.0f, 0.0f );
+        dir = Quaternion.AngleAxis(roll, Vector3.up) * vec;
+        Debug.Log(name + "'s dir:" + dir);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    void Update () {
+        float roll = transform.localRotation.y;
+        Vector3 vec = new Vector3(speed, 0.0f, 0.0f);
+        dir = Quaternion.AngleAxis(roll, Vector3.up) * vec;
+        Debug.Log(name + "'s dir:" + roll + "," + dir);
 		
 	}
 
@@ -22,14 +30,16 @@ public class Conveyor : MonoBehaviour {
 
     void OnTriggerStay ( Collider col ) {
         Transform t = col.gameObject.transform;
-        float diffZ = t.localPosition.z - transform.localPosition.z;
+        /*
+        float diffCentor = t.localPosition.z - transform.localPosition.z;
         float newZ = 0.0f;
-        if( diffZ > 0 ){
+        if( diffCentor > 0 ){
             newZ = -speed;
         }
-        else if( diffZ < 0 ){
+        else if( diffCentor < 0 ){
             newZ = speed;
         }
-        t.localPosition += new Vector3( speed, 0, newZ );
+        */
+        t.localPosition += dir;
     }
 }
