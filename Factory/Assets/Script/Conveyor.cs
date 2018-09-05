0f798066224bd9ac7a3d8d39992a2fc2d39d2209
tamiwo <tamiwo.otutama@gmail.com>
2018-09-05 19:20:07 +0900

    fix:conveyerの回転に合わせた方向に進むように

--- Factory/Assets/Script/Conveyor.cs
+++ Factory/Assets/Script/Conveyor.cs
@@ -9,18 +9,13 @@ public class Conveyor : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        float roll = transform.localRotation.y;
         Vector3 vec = new Vector3( speed, 0.0f, 0.0f );
-        dir = Quaternion.AngleAxis(roll, Vector3.up) * vec;
+        dir = Quaternion.Euler(transform.localRotation.eulerAngles) * vec;
         Debug.Log(name + "'s dir:" + dir);
 	}
 	
 	// Update is called once per frame
     void Update () {
-        float roll = transform.localRotation.y;
-        Vector3 vec = new Vector3(speed, 0.0f, 0.0f);
-        dir = Quaternion.AngleAxis(roll, Vector3.up) * vec;
-        Debug.Log(name + "'s dir:" + roll + "," + dir);
 		
 	}
 
