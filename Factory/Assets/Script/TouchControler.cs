3b4661bc306f79ab8eeff2bb3b88301e769b2558
tamiwo <tamiwo.otutama@gmail.com>
2018-09-11 12:47:26 +0900

    update:スクロール処理追加
    Sceneには空オブジェクトを追加して
    スクリプトをaddして
    カメラを設定する

---
+++ Factory/Assets/Script/TouchControler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControler : MonoBehaviour {
+
+    private Vector3 touchPos;
+    private bool isTouching;      // タッチ中かな？
+    public Camera cam;              // カメラ(外から入れる)
+
+	// Use this for initialization
+	void Start () {
+        isTouching = false;
+        touchPos = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Scroll();
+    }
+
+    // スクロール処理
+    private void Scroll() {
+        Vector3 newTouchPos;
+        
+        // タッチ中
+        if(isTouching) {
+            // 今回の座標を取得
+            newTouchPos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+            // 前回の座標と違ったら
+            if (touchPos != newTouchPos) {
+                // カメラをその分動かす
+                cam.transform.position -= newTouchPos - touchPos;
+            }
+
+            // 今回の座標を保存
+            touchPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        // タッチ開始
+        if (Input.GetMouseButtonDown(0)) {
+            isTouching = true;
+            // 今回の座標を保存
+            touchPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        // タッチ終了
+        if (Input.GetMouseButtonUp(0)) {
+            isTouching = false;
+            touchPos = new Vector3(0, 0, 0);
+        }
+    }
+
+}
