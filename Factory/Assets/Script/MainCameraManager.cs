eb1d1a6382bda9b2169c983f36f92ffe32ea24c3
Tamiwo <taimwo.otutama@gmail.com>
2018-09-14 17:40:58 +0900

    update:カメラサイズ調整

--- Factory/Assets/Script/MainCameraManager.cs
+++ Factory/Assets/Script/MainCameraManager.cs
@@ -27,5 +27,23 @@ public class MainCameraManager : MonoBehaviour
         var cameraScreenPosition = myCamera.WorldToScreenPoint(myCamera.transform.position);
         var newCameraScreenPosition = cameraScreenPosition - deltaScreenPosition;
         myCamera.transform.position = myCamera.ScreenToWorldPoint(newCameraScreenPosition);
+
+        SetCamaraZoom(1/PanGesture.DeltaScale);
+
+    }
+
+    void SetCamaraZoom(float scale){
+
+        float max = 2.0f;
+        float min = 1 / 4;
+
+        if ( scale > max ) {
+            scale = max;
+        }
+        else if ( scale < min ) {
+            scale = min;
+        }
+        myCamera.orthographicSize *= scale;
+
     }
 }
