5297a8de0b177debbef9aa02d37e36fa25d67d05
Tamiwo <taimwo.otutama@gmail.com>
2018-09-14 17:19:58 +0900

    update:Touchスクリプトで実装し直し

---
+++ Factory/Assets/Script/MainCameraManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using TouchScript.Gestures.TransformGestures;
+using UnityEngine;
+
+public class MainCameraManager : MonoBehaviour
+{
+    private ScreenTransformGesture PanGesture;
+    private Camera myCamera;
+
+    private void OnEnable()
+    {
+        myCamera = GetComponent<Camera>();
+        PanGesture = GetComponent<ScreenTransformGesture>();
+        PanGesture.Transformed += Pan;
+    }
+
+    private void OnDisable()
+    {
+        PanGesture.Transformed -= Pan;
+    }
+
+    private void Pan(object sender, System.EventArgs e)
+    {
+        // Convert touch and camera positions to a common coordinate system.
+        var deltaScreenPosition = PanGesture.DeltaPosition;
+        var cameraScreenPosition = myCamera.WorldToScreenPoint(myCamera.transform.position);
+        var newCameraScreenPosition = cameraScreenPosition - deltaScreenPosition;
+        myCamera.transform.position = myCamera.ScreenToWorldPoint(newCameraScreenPosition);
+    }
+}
