aa99994ea6a8aa7e6655d893a9db6eb2d457767a
tamiwo <tamiwo.otutama@gmail.com>
2020-02-01 18:13:05 +0900

    update:ボーンを入れて手を動かしてみる

---
+++ Factory/Assets/bunbun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bunbun : MonoBehaviour
+{
+
+    public float max;
+    public float min;
+
+    public Vector3 speedABS;
+    private Vector3 speed;
+    public Vector3 angle;
+
+    private void Start()
+    {
+        speed = speedABS;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(speed*Time.deltaTime);
+
+        angle= transform.localEulerAngles;
+        if( angle.z > 180 ) angle.z-=360;
+        if (angle.z > max)
+        {
+            speed = -speedABS;
+        }
+        else if (angle.z < min)
+        {
+            speed = speedABS;
+        }
+
+    }
+}
